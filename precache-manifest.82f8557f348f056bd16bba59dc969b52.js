a57020d73ea59981428dab7ec52d259eb41048e6
Carolyn Hasselkus <carolyn.hasselkus@MacBook-Pro.attlocal.net>
2020-07-07 20:21:49 -0500

    Updates

---
+++ precache-manifest.82f8557f348f056bd16bba59dc969b52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46fa23464427b8c3881a1c5bf7adfd6",
+    "url": "/text-face-generator/index.html"
+  },
+  {
+    "revision": "a6b4699be3981cbc76d8",
+    "url": "/text-face-generator/static/css/main.7a006ebc.chunk.css"
+  },
+  {
+    "revision": "c955c07e47a9a3527f54",
+    "url": "/text-face-generator/static/js/2.96a61231.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/text-face-generator/static/js/2.96a61231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b4699be3981cbc76d8",
+    "url": "/text-face-generator/static/js/main.1bb8ab29.chunk.js"
+  },
+  {
+    "revision": "3df64b0900691a7e459d",
+    "url": "/text-face-generator/static/js/runtime-main.84d833d9.js"
+  }
+]);
